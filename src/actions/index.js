1bd474b3d67e90c3da8f49f49e01bb6acfd562ff
Leandro Marques <leandrolmarques@gmail.com>
2017-07-10 05:38:29 -0300

    Section 5.57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'a2d3d5218a51c17ebbb8ee2d66994393';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: ROOT_URL,
+    payload: request
+  };
+}
